4d5a2c6175e2b7b3692c922eacaef1b3433eab2a
hardchisel <chris@heald.com>
2015-12-01 12:42:16 +0000

    array search algorithm started

--- ExcelAccounting/Class1.cs
+++ ExcelAccounting/Class1.cs
@@ -24,15 +24,26 @@ namespace ExcelAccounting
         }
 
         [ExcelFunction(Description = "My first .NET function")]
-        public static string XA_Price(object[,] array)
+        public static string XA_Price(object[,] array, string asset_code)
         {
-            //var caller = Excel(xlfCaller) as ExcelReference;
-            //if (caller == null)
-            //    return array;
-
             int rows = array.GetLength(0);
-            int columns = array.GetLength(1);
-            return String.Format("Hello {0} {1}", rows, columns);
+            int cols = array.GetLength(1);
+
+            int x = -1;
+            for (int col = 0; col < cols; col++)
+            {
+                dynamic col_name = array[0, col];
+                if (col_name != null && col_name is string && col_name == asset_code)
+                {
+                    for (int row = 1; row < rows; row++)
+                    {
+                        dynamic row_name = array[row, 0];
+                    }
+                    break;
+                }
+            }
+
+            return String.Format("{0} found at {1}", asset_code, x);
         }
     }
 }
