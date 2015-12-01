409789c4332685b5453f23fd69fafb258719e746
hardchisel <chris@heald.com>
2015-12-01 18:29:56 +0000

    XA_Price working

--- ExcelAccounting/Class1.cs
+++ ExcelAccounting/Class1.cs
@@ -23,27 +23,53 @@ namespace ExcelAccounting
             return "Hello " + name + "!";
         }
 
-        [ExcelFunction(Description = "My first .NET function")]
-        public static string XA_Price(object[,] array, string asset_code)
+        [ExcelFunction(Description = "Get asset price at specified date")]
+        public static double XA_Price(object[,] array, string asset_code, double price_date)
         {
             int rows = array.GetLength(0);
             int cols = array.GetLength(1);
 
-            int x = -1;
+            double price = 0;
             for (int col = 0; col < cols; col++)
             {
+                // check each column header (asset code)
                 dynamic col_name = array[0, col];
                 if (col_name != null && col_name is string && col_name == asset_code)
                 {
+                    // found a matching column, work down rows
                     for (int row = 1; row < rows; row++)
                     {
-                        dynamic row_name = array[row, 0];
+                        // check each row's date
+                        dynamic row_date = array[row, 0];
+                        if (row_date != null && row_date is double && row_date <= price_date)
+                        {
+                            // check if we exceeded requested date
+                            if (row_date > price_date)
+                                break;
+                            else
+                            // found a candidate row
+                            {
+                                if (row == (rows - 1) && price_date > row_date)
+                                {
+                                    // we requested a future date
+                                    price = 0;
+                                    break;
+                                }
+                                else
+                                {
+                                    // this could be the price
+                                    dynamic price_candidate = array[row, col];
+                                    if (price_candidate != null && price_candidate is double)
+                                        price = price_candidate;
+                                }
+                            }
+                        }
                     }
                     break;
                 }
             }
 
-            return String.Format("{0} found at {1}", asset_code, x);
+            return price;
         }
     }
 }
