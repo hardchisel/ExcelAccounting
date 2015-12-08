dbd25f44a253ab29c141a78b29edd0899ee1470d
Chris Heald <chris@heald.com>
2015-12-08 14:57:56 +0000

    Started XA_Value function

--- ExcelAccounting/Class1.cs
+++ ExcelAccounting/Class1.cs
@@ -24,23 +24,23 @@ namespace ExcelAccounting
         }
 
         [ExcelFunction(Description = "Get asset price at specified date")]
-        public static double XA_Price(object[,] array, string asset_code, double price_date)
+        public static double XA_Price(object[,] price_array, string asset_code, double price_date)
         {
-            int rows = array.GetLength(0);
-            int cols = array.GetLength(1);
+            int rows = price_array.GetLength(0);
+            int cols = price_array.GetLength(1);
 
             double price = 0;
             for (int col = 0; col < cols; col++)
             {
                 // check each column header (asset code)
-                dynamic col_name = array[0, col];
+                dynamic col_name = price_array[0, col];
                 if (col_name != null && col_name is string && col_name == asset_code)
                 {
                     // found a matching column, work down rows
                     for (int row = 1; row < rows; row++)
                     {
                         // check each row's date
-                        dynamic row_date = array[row, 0];
+                        dynamic row_date = price_array[row, 0];
                         if (row_date != null && row_date is double && row_date <= price_date)
                         {
                             // check if we exceeded requested date
@@ -57,8 +57,8 @@ namespace ExcelAccounting
                                 }
                                 else
                                 {
-                                    // this could be the price
-                                    dynamic price_candidate = array[row, col];
+                                    // this could be the price, so save it
+                                    dynamic price_candidate = price_array[row, col];
                                     if (price_candidate != null && price_candidate is double)
                                         price = price_candidate;
                                 }
@@ -70,6 +70,12 @@ namespace ExcelAccounting
             }
 
             return price;
+        }
+
+        [ExcelFunction(Description = "Get asset value at specified date")]
+        public static double XA_Value(object[,] asset_table, object[,] price_array, string asset_code, string value_asset_code, double price_date)
+        {
+            return 0;
         }
     }
 }
